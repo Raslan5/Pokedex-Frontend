e53e18324bdeaeccca278f23cd96fcdbce57fa1a
Mohamad Raslan <Refez.25@outlook.com>
2025-03-21 19:19:39 +0200

    Testing

--- src/components/pokemonTypeBadgeComp.tsx
+++ src/components/pokemonTypeBadgeComp.tsx
@@ -5,26 +5,26 @@ import { Badge } from "react-bootstrap";
 
 
 interface PokemonCardCompProps {
-   pokemonTypes: String[];
+    pokemonTypes: String[];
 }
 
 
 export default function PokemonTypeBadgeComp(props: PokemonCardCompProps) {
-   return (
-       <>
-           {props.pokemonTypes?.map((pokemonType, index) => {
-               if (pokemonType === "Water") {
-                   return <Badge key={index} bg="primary">{pokemonType}</Badge>
-               } else if (pokemonType === "Fire") {
-                   return <Badge key={index} bg="danger">{pokemonType}</Badge>
-               } else if (pokemonType === "Grass") {
-                   return <Badge key={index} bg="success">{pokemonType}</Badge>
-               } else if (pokemonType === "Electric") {
-                   return <Badge key={index} bg="warning">{pokemonType}</Badge>
-               } else {
-                   return <Badge key={index} bg="secondary">{pokemonType}</Badge>
-               }
-           })}
-       </>
-   );
+    return (
+        <>
+            {props.pokemonTypes?.map((pokemonType, index) => {
+                if (pokemonType === "Water") {
+                    return <Badge key={index} bg="primary">{pokemonType}</Badge>
+                } else if (pokemonType === "Fire") {
+                    return <Badge key={index} bg="danger">{pokemonType}</Badge>
+                } else if (pokemonType === "Grass") {
+                    return <Badge key={index} bg="success">{pokemonType}</Badge>
+                } else if (pokemonType === "Electrical!") {
+                    return <Badge key={index} bg="warning">{pokemonType}</Badge>
+                } else {
+                    return <Badge key={index} bg="secondary">{pokemonType}</Badge>
+                }
+            })}
+        </>
+    );
 }
